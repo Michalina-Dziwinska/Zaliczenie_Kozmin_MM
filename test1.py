c17191292fce092dd5f68503728481550f489ec5
Michalina <michalina.dziwinska@gmail.com>
2024-06-09 13:57:41 +0200

    dodaję trzy def

--- test1.py
+++ test1.py
@@ -1,9 +1,19 @@
 from selenium import webdriver
 import chromedriver_autoinstaller
 
-
 chromedriver_autoinstaller.install()
 
-driver = webdriver.Chrome()
-driver.get("https://www.kiwi.com/pl/")
-assert "Python" in driver.title
+def test_is_kiwi_successfully_loaded_1():
+    driver = webdriver.Chrome()
+    driver.get("https://www.kiwi.com/pl/")
+    assert "Kiwi.com | Znajdź tanie loty i odkryj nowe miejsca" in driver.title
+
+def test_is_kiwi_successfully_loaded_2():
+    driver = webdriver.Chrome()
+    driver.get("https://www.kiwi.com/pl/")
+    assert "Kiwi.com | Znajdź tanie loty i odkryj nowe miejsca" in driver.title
+
+def test_is_kiwi_successfully_loaded_3():
+    driver = webdriver.Chrome()
+    driver.get("https://www.kiwi.com/pl/")
+    assert "Kiwi.com | Znajdź tanie loty i odkryj nowe miejsca" in driver.title
