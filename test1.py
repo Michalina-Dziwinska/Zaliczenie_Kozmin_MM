42c79301d38fcd976b0a10ca68e83275c2efaa0b
Michalina <michalina.dziwinska@gmail.com>
2024-06-10 17:03:51 +0200

    doszłam do kliknięcia w hamburgera na głownej

--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 chromedriver_autoinstaller.install()
 
-def test_is_kiwi_successfully_loaded_1():
+def registration():
     driver = webdriver.Chrome()
     driver.get("https://www.kiwi.com/pl/")
     driver.find_element(By.CSS_SELECTOR, '[data-test="CookiesPopup-Accept"]').click()
