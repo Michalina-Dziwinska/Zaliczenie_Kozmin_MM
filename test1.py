43316ce7d68f954ab06c03fdffe52ac262135007
Michalina <michalina.dziwinska@gmail.com>
2024-06-10 17:02:04 +0200

    doszłam do kliknięcia w hamburgera na głownej

--- test1.py
+++ test1.py
@@ -1,19 +1,30 @@
 from selenium import webdriver
 import chromedriver_autoinstaller
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 chromedriver_autoinstaller.install()
 
 def test_is_kiwi_successfully_loaded_1():
     driver = webdriver.Chrome()
     driver.get("https://www.kiwi.com/pl/")
-    assert "Kiwi.com | Znajdź tanie loty i odkryj nowe miejsca" in driver.title
+    driver.find_element(By.CSS_SELECTOR, '[data-test="CookiesPopup-Accept"]').click()
+    print("cookisy ok")
+    #cookie_button.click()
+    driver.find_element(By.CSS_SELECTOR, '[data-test="TopNav-SideNav-Open"]').click()
+    print("hamburger kliknięty")
+
+    #assert "Kiwi.com | Znajdź tanie loty i odkryj nowe miejsca" in driver.title
 
 def test_is_kiwi_successfully_loaded_2():
     driver = webdriver.Chrome()
     driver.get("https://www.kiwi.com/pl/")
     assert "Kiwi.com | Znajdź tanie loty i odkryj nowe miejsca" in driver.title
 
-def test_is_kiwi_successfully_loaded_3():
+def click_top_nav():
     driver = webdriver.Chrome()
     driver.get("https://www.kiwi.com/pl/")
-    assert "Kiwi.com | Znajdź tanie loty i odkryj nowe miejsca" in driver.title
+    hamburger_button = element_by_attribute = driver.find_element(By.CSS_SELECTOR, '[data-test="TopNav-SideNav-Open"]')
+    hamburger_button.click()
+    assert "Udało się"
