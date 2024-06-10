db5206eba6855186976dcceeaa78cfb8b04ca427
Michalina <michalina.dziwinska@gmail.com>
2024-06-10 17:38:04 +0200

    czytowpadnienamichaline

--- test1.py
+++ test1.py
@@ -6,7 +6,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 chromedriver_autoinstaller.install()
 
-def registration():
+
+def test_registration():
     driver = webdriver.Chrome()
     driver.get("https://www.kiwi.com/pl/")
     driver.find_element(By.CSS_SELECTOR, '[data-test="CookiesPopup-Accept"]').click()
@@ -14,13 +15,14 @@ def registration():
     #cookie_button.click()
     driver.find_element(By.CSS_SELECTOR, '[data-test="TopNav-SideNav-Open"]').click()
     print("hamburger kliknięty")
-
     #assert "Kiwi.com | Znajdź tanie loty i odkryj nowe miejsca" in driver.title
+
 
 def test_is_kiwi_successfully_loaded_2():
     driver = webdriver.Chrome()
     driver.get("https://www.kiwi.com/pl/")
     assert "Kiwi.com | Znajdź tanie loty i odkryj nowe miejsca" in driver.title
+
 
 def click_top_nav_UWAGAnieDzialaTo():
     driver = webdriver.Chrome()
