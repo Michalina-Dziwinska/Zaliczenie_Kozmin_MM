2c4bd6b8651cf0320160c3058d413d2cd2e94751
Michalina <michalina.dziwinska@gmail.com>
2024-06-10 20:26:05 +0200

    koncowa wersja z Asia

--- test1.py
+++ test1.py
@@ -1,23 +1,19 @@
 from _pydatetime import time
-
 from selenium import webdriver
 import chromedriver_autoinstaller
-from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
-
-chromedriver_autoinstaller.install()
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.home_page import HomePage
 
 
 def test_registration():
     driver = webdriver.Chrome()
     driver.get("https://www.kiwi.com/pl/")
-    driver.find_element(By.CSS_SELECTOR, '[data-test="CookiesPopup-Accept"]').click()
-    print("cookisy ok")
-    #cookie_button.click()
+    home_page = HomePage(driver)
+    home_page.accept_cookies_policy()
     driver.find_element(By.CSS_SELECTOR, '[data-test="TopNav-SideNav-Open"]').click()
-    print("hamburger kliknięty")
     driver.find_element(By.XPATH, "//a[contains(text(), 'Utwórz konto')]")
-    print("utworz konto klikniete")
 
 
 def test_is_kiwi_successfully_loaded_2():
@@ -29,14 +25,21 @@ def test_is_kiwi_successfully_loaded_2():
 def test_click_on_login_button():
     driver = webdriver.Chrome()
     driver.get("https://www.kiwi.com/pl/")
-    login_button = driver.find_element(By.XPATH, '//*[@id="react-view"]/div[2]/div[2]/div/aside/div[2]/div[1]/button')
-    login_button.click()
+    driver.find_element(By.CSS_SELECTOR, '[data-test="CookiesPopup-Accept"]').click()
+    driver.find_element(By.CSS_SELECTOR, '[data-test="TopNav-SideNav-Open"]').click()
+    manager = WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//aside//button[./*[text()='Zaloguj się']]")))
+    manager.click()
     assert "Użytkownik przeszedł do formularza logowania"
 
 
 def test_click_on_alternative_login_method():
     driver = webdriver.Chrome()
     driver.get("https://www.kiwi.com/pl/")
-    alternative_button = driver.find_element(By.XPATH, '')
+    driver.find_element(By.CSS_SELECTOR, '[data-test="CookiesPopup-Accept"]').click()
+    driver.find_element(By.CSS_SELECTOR, '[data-test="TopNav-SideNav-Open"]').click()
+    manager = WebDriverWait(driver, 40).until(
+    EC.element_to_be_clickable((By.XPATH, "//aside//button[./*[text()='Zaloguj się']]")))
+    manager.click()
+    alternative_button = driver.find_element(By.XPATH, "//a[@data-test='MagicLogin-IncorrectEmail']")
     alternative_button.click()
     assert "Użytkownik przeszedł do alternatywnej metody logowania"
