8187804db414abc4e41db194bd2e20a5b3c138d4
Michalina <michalina.dziwinska@gmail.com>
2024-06-12 21:21:47 +0200

    zmiany na miske

--- pages/offers_page.py
+++ pages/offers_page.py
@@ -1,8 +1,9 @@
 import time
-
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class OffersPage:
@@ -10,15 +11,30 @@ class OffersPage:
         self.driver = driver
 
     def clean_placepicker_input(self):
-        self.driver.find_element(By.CSS_SELECTOR, '[data-test="PlacePickerInputPlace-close"]').click()
+        place_picker_wait = (WebDriverWait(self.driver, 10).until
+         (EC.visibility_of_element_located((By.CSS_SELECTOR, '[data-test="PlacePickerInputPlace-close"]'))))
+        place_picker_wait.click()
+
+    def clean_placepicker_input2(self):
+        input_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[data-test="SearchField-input"]')))
+        input_element.send_keys(Keys.CONTROL + 'a')
+        input_element.send_keys(Keys.BACKSPACE)
+        input_element.send_keys(Keys.BACKSPACE)
 
 
     def add_country_input(self):
         self.driver.find_element(By.CSS_SELECTOR, '[data-test="SearchField - input"]').click()
 
-
     def search_country(self):
-        input_element = self.driver.find_element(By.CSS_SELECTOR, '[data-test="SearchField - input"]').click()
+        input_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[data-test="SearchField-input"]'))
+        )
+        input_element.click()
         input_element.send_keys("Korea Południowa")
-        time.sleep(4)
-        #input_element.send_keys(Keys.ENTER)
+
+
+    def empty_place_click(self):
+        empty_space = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[data-test="newSearchForm"]')))
+        empty_space.click()
